e8836a6a12726905ad7b0bcd7920c223de3f6439
Cody K <ckietzm2@gmail.com>
2023-10-05 14:31:14 -0500

    erased .

--- assets/js/script.js
+++ assets/js/script.js
@@ -278,7 +278,7 @@ parkSelected.style.display = "block";
 }
 
   function fetchForecastData() {
-.
+
 
     var apiKey = "a10bc788276a7c7ca6f89df126f2779a";
     var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
